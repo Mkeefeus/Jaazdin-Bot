3df45b05fa6bd90439f4de31d803d28b05aba399
Malcolm Keefe <Mkeefeus@gmail.com>
2025-10-04 19:56:11 -0400

    Remove ssl requirement

--- src/db/db.ts
+++ src/db/db.ts
@@ -9,12 +9,6 @@ const dbsettings: Partial<Options> = {
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
 };
 
 export const db =
